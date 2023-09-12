99c7592ecd0ef6213f9ef8da8481a2b4d373cd74
Prakash <upadhayaypc@gmail.com>
2023-09-12 15:16:38 -0400

    Added Hero Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Hero />
       <h1>This is React!</h1>
     </>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+function Hero() {
+  return (
+    <section>
+      <h1 className="hero--header">Online Experiences</h1>
+      <p className="hero--text">
+        Join unique interactive activities led by one-of-a-kind hosts-all
+        without leaving home.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
